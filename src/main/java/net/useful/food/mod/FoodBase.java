183679299be5dd5249d0ccd60a51a09be87a3a2a
licaiwang99 <licaiwang99@gmail.com>
2020-02-02 17:26:48 +0800

    docs(change model name):

---
+++ src/main/java/net/useful/food/mod/FoodBase.java
@@ -0,0 +1,10 @@
+package net.useful.food.mod;
+import net.minecraft.item.Item;
+
+public class FoodBase extends Item{
+
+    public FoodBase(Settings settings) {
+        super(settings);
+    }
+
+}
